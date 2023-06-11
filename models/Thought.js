29cf548b736debb9308e8a5b698db566e701e7c9
Jegaco <jeffgcorpuz@gmail.com>
2023-06-10 19:57:26 -0700

    Created User, Thought and Reaction models

---
+++ models/Thought.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require('mongoose'); 
+const reactionSchema = require('./Reaction');
+const Thought = model('Thought',thoughtSchema)
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText:{
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlenght: 280,
+        },
+        createdAt:{
+            type: Date,
+            default: Date.now,
+            get: timeCreated => new Date(timeCreated).toLocaleString(),
+        },
+        username:{
+            type: String,  
+            required: true,
+        },
+        reactions:[reactionSchema]
+    },
+    {
+        toJSON: {
+            getters: true,
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function(){
+    return this.reactions.length;
+});
+
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { Schema, model, Types } = require('mongoose'); 
+const User = model('User',userSchema)
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+      
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/]
+    },
+    thoughts:[
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'Thought',
+      }
+    ],
+    friends:[
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+    }
+  ],
+    
+  },
+  {
+    toJSON: {
+      virtuals: true, 
+    },
+    id: false,
+}
+);
+
+userSchema.virtual('friendCount').get(function(){
+    return this.friends.length;
+});
+
+module.exports = User;
